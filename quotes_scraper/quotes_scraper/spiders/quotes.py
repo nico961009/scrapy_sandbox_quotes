4958a21e7584500d37b46ef3c9e68265780dc43d
NickViajero <andradenicolas76@gmail.com>
2021-12-17 13:38:35 -0600

    Se modificó el spider para que fuera capaz de pasar a la siguiente página y seguir extrayendo las quotes, sin embargo nos encontramos con un 'erro' molesto que fue que también extrajó nuevamente el título y el 10 top tags cada vez que cambiaba de página lo cual no era deseable

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,6 +3,7 @@ import scrapy
 # Título = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags= //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button=//ul[@class="pager"]//li[@class="next"]/a/@href
 
 
 class QuotesSpider(scrapy.Spider):
@@ -11,6 +12,12 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'http://quotes.toscrape.com/'
     ]
+    # Generamos un atributo de configuración para automátizar algunos procesos como el guardar
+    # el output en un archivo .json:
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
@@ -27,3 +34,15 @@ class QuotesSpider(scrapy.Spider):
             'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
+
+        # Para poder cambiar de página dando click en el botón
+        # primero asignaremos la ruta XPath a una variable:
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+
+        # Ahora preguntamos si este botón efectivamente existe (en la última
+        # página no existirá)
+        if next_page_button_link:
+            # El método follow lleva dos parámetros: link a seguir y un callback que nos dice que vamos
+            # a hacer con el link que estamos siguiendo.
+            yield response.follow(next_page_button_link, callback=self.parse)
