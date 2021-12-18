7c76c861a9ce5b8338564589054373f67b05845e
NickViajero <andradenicolas76@gmail.com>
2021-12-18 13:19:00 -0600

    En esta sesion se agregaron configuraciones al atributo 'custom_settings' que nos serán muy útilies en nuestros proyectos

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -11,9 +11,29 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'http://quotes.toscrape.com/'
     ]
+    # Colocamos otras configuraciones para sacar el máximo jugo a este framework
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        'FEED_FORMAT': 'json',
+        # Esta conf nos permitira definir un número entero que le dirá a scrapy el número de peticiones
+        # teniendo en cuanta este número definido:
+        'CONCURRENT_REQUESTS': 24,
+        # Podemos definir la cantidad de memoria RAM que le permitimos usar a Scrapy para trabajar,
+        # esto es super útil si tenemos a nuestro scrapper corriendo en la nube para no sobre cargar a nuetro
+        # servidor si tenemos muchos  procesos corriendo y no alentar otros servicios que tengamos.
+        'MEMUSAGE_LIMIT_MB': 2048,
+        # Ahora si se pasa el uso de memoria RAM asignada podemos notificar al administración mediante el siguiente
+        # atributo (en este puedo colocar un email para notificar):
+        # Al ser una lista puedes asignar más emails.
+        'MEMUSAGE_NOTIFY_MAIL': ['beto96_tuzhdez@hotmail.com'],
+        # Decirle si va obedecer al archivo robots.txt de la página que estamos scrappeando. Este atributo es recomendable
+        # tenerlo siempre activado para evitarnos problemas legales.
+        'ROBOTSTXT_OBEY': True,
+        # Se puede cambiar el user-agent mediante el siguiente atributo:
+        'USER_AGENT': 'Spider_scrapy_curso_PLATZI',
+        # Podemos cambiar el encoding del archivo para que no nos de errores en los carácteres, esto lo debemos de hacer
+        # mediante el siguiente atributo:
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def parse_only_quotes(self, response, **kwargs):
@@ -38,17 +58,9 @@ class QuotesSpider(scrapy.Spider):
         top_tags = response.xpath(
             '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
 
-        # Mediante esta línea de código vamos de una manera simple traer un número determinado de citas o tags:
-        # Generamos una variable donde le preguntamos los siguiente a Scrapy:
-        # Si existe dentro de la ejecución de este spider un atributo llamado "top" voy a guardar lo que este dentro
-        # de este atributo en la variable 'top', ahora que si este atributo no existe lo que guardaré en mi variable
-        # será "None".
         top = getattr(self, 'top', None)
-        # Ahora decimos, si existe top haré lo siguiente:
         if top:
             top = int(top)
-            # Ahora lo que guardaré en mi variable de top_tags será, mediante slices de python, desde el primer
-            # índice hasta "top" que será el valor que nosostros definamos.
             top_tags = top_tags[:top]
 
         yield {
