d8f0cd1c1d456f200c410f36066d3cfea85b637b
NickViajero <andradenicolas76@gmail.com>
2021-12-18 12:44:59 -0600

    En esta sesión se aprendio como pasarle argumentos a nuestro spider a través de consola para traer un número detrminado (por nosotros en consola) de tags

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -2,7 +2,7 @@ import scrapy
 
 # Título = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
-# Top ten tags= //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Top tags= //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
 # Next page button=//ul[@class="pager"]//li[@class="next"]/a/@href
 
 
@@ -11,28 +11,19 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'http://quotes.toscrape.com/'
     ]
-    # Generamos un atributo de configuración para automátizar algunos procesos como el guardar
-    # el output en un archivo .json:
     custom_settings = {
         'FEED_URI': 'quotes.json',
         'FEED_FORMAT': 'json'
     }
 
-    # creamos un nuevo método tipo parse para extraer únicamente las citas sin títulos
-    # ni top ten tags
-    # Como argumentos tendrá self (por ser un método de una clase) y response (respuesta del servidor).
-    # Le agregamos el argumeto **kwargs para desempaquetar el diccionario
     def parse_only_quotes(self, response, **kwargs):
-        # Verificamos que hayamos recibido kwargs
         if kwargs:
-            # Guardamos en una variable local lo que contenga este diccionario.
             quotes = kwargs['quotes']
-            # Se coloca entre corchetes el nombre que definimos en la parte de response.follow() en este caso fue 'quotes'.
         quotes.extend(response.xpath(
             '//span[@class="text" and @itemprop="text"]/text()').getall())
-        # Ahora vamos con la tercer página y para esto preguntamos si existe un botón de next.
         next_page_button_link = response.xpath(
             '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+
         if next_page_button_link:
             yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
         else:
@@ -44,26 +35,29 @@ class QuotesSpider(scrapy.Spider):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath(
             '//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath(
+        top_tags = response.xpath(
             '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
 
-        # Con la keyword yield convierto mi método en un generador.
-        # Y lo que haré será devolver un diccionario para poder hacer mi transformación
-        # fácilmente.
+        # Mediante esta línea de código vamos de una manera simple traer un número determinado de citas o tags:
+        # Generamos una variable donde le preguntamos los siguiente a Scrapy:
+        # Si existe dentro de la ejecución de este spider un atributo llamado "top" voy a guardar lo que este dentro
+        # de este atributo en la variable 'top', ahora que si este atributo no existe lo que guardaré en mi variable
+        # será "None".
+        top = getattr(self, 'top', None)
+        # Ahora decimos, si existe top haré lo siguiente:
+        if top:
+            top = int(top)
+            # Ahora lo que guardaré en mi variable de top_tags será, mediante slices de python, desde el primer
+            # índice hasta "top" que será el valor que nosostros definamos.
+            top_tags = top_tags[:top]
+
         yield {
             'title': title,
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
-        # Para poder cambiar de página dando click en el botón
-        # primero asignaremos la ruta XPath a una variable:
         next_page_button_link = response.xpath(
             '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
 
-        # Ahora preguntamos si este botón efectivamente existe (en la última
-        # página no existirá)
         if next_page_button_link:
-            # El método follow lleva dos parámetros: link a seguir y un callback que nos dice que vamos
-            # a hacer con el link que estamos siguiendo.
             yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
-            # Agregamos el argumento cb_kwargs=quotes para pasarlo al metodo que hace referencia el callback.
